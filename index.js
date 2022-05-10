5132265c866dafce8fa47ce55455aa1d74bebcd2
Vivipe28 <viviana.perez28@gmail.com>
2022-05-10 11:50:35 -0500

    md-links

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-module.exports = () => {
-  // ...
-};
+const {program} = require('commander');
+
+program.version('0.0.1').description('A command line toll for managing md-links');
+
+program.parse(process.argv)
